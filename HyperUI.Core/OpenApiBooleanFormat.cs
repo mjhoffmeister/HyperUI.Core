ce92e4d9fdef862f68938bc91fbe6fa3453245e9
Matt Hoffmeister <matt.hoffmeister@gmail.com>
2022-07-20 12:27:07 -0500

    Add boolean format

---
+++ HyperUI.Core/OpenApiBooleanFormat.cs
@@ -0,0 +1,17 @@
+﻿namespace HyperUI.Core;
+
+/// <summary>
+/// OpenAPI boolean format.
+/// </summary>
+public static class OpenApiBooleanFormat
+{
+    /// <summary>
+    /// Choice.
+    /// </summary>
+    public static readonly string Choice = "choice";
+
+    /// <summary>
+    /// Toggle.
+    /// </summary>
+    public static readonly string Toggle = "toggle";
+}
